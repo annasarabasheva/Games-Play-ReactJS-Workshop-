40d78a92fcde51422806715c94abd0f66b1b7f5c
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-07-12 15:01:02 +0300

    Add Register and Login components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import GameCreate from "./components/game-create/GameCreate"
 import GameDetails from "./components/game-details/GameDetails"
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
+import Login from "./components/login/Login"
+import Register from "./components/register/Register"
 import { Route, Routes } from "react-router-dom"
 
 function App() {
@@ -19,6 +21,8 @@ function App() {
             <Route path="/games" element={<Catalogue/>}/>
             <Route path="/games/create" element={<GameCreate/>}/>
             <Route path="/games/details/:gameID" element={<GameDetails/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
         </Routes>
 
 

3d22f151b4a452bb39fbd3354e1a1752bf94e6a9
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-07-23 13:40:45 +0300

    Add auth context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Catalogue from "./components/catalogue/Catalog"
 import GameCreate from "./components/game-create/GameCreate"
 import GameDetails from "./components/game-details/GameDetails"
@@ -6,27 +7,35 @@ import Home from "./components/home/Home"
 import Login from "./components/login/Login"
 import Register from "./components/register/Register"
 import { Route, Routes } from "react-router-dom"
+import AuthContext from "./contexts/authContext"
 
 function App() {
- 
+    const [auth, setAuth] = useState({});
 
-  return (
+    const loginSubmitHandler = (values) => {
+        console.log(values)
+    }
 
-    <div id="box">
+    return (
+        
+        <AuthContext.Provider value={{loginSubmitHandler}}>
+            <div id="box">
 
-        <Header/>
+                <Header/>
 
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/games" element={<Catalogue/>}/>
-            <Route path="/games/create" element={<GameCreate/>}/>
-            <Route path="/games/details/:gameID" element={<GameDetails/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
-        </Routes>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/games" element={<Catalogue/>}/>
+                    <Route path="/games/create" element={<GameCreate/>}/>
+                    <Route path="/games/details/:gameID" element={<GameDetails/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                </Routes>
 
 
-    </div>
+            </div>
+        </AuthContext.Provider>
+       
 
   )
 }
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import useForm from "../../hooks/useForm";
+import AuthContext from "../../contexts/authContext";
 
 const LoginFormKeys = {
     Email: 'email',
@@ -7,9 +9,13 @@ const LoginFormKeys = {
 
 
 export default function Login() {
-    const {values, onChange, onSubmit} = useForm(
+
+    const {loginSubmitHandler} = useContext(AuthContext)
+
+    const {values, onChange, onSubmit} = useForm(loginSubmitHandler,
         {[LoginFormKeys.Email]: '', [LoginFormKeys.Password]: ''}
     )
+
     return (
         <section id="login-page" className="auth">
             <form id="login" onSubmit={onSubmit}>

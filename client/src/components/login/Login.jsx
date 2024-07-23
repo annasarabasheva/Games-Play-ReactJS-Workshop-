a4d573c5e2b2632edcfe625c96d3d730ead4aacc
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-07-23 13:20:40 +0300

    Add login form keys

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,8 +1,14 @@
 import useForm from "../../hooks/useForm";
 
+const LoginFormKeys = {
+    Email: 'email',
+    Password: 'password'
+}
+
+
 export default function Login() {
     const {values, onChange, onSubmit} = useForm(
-        {email: '', password: ''}
+        {[LoginFormKeys.Email]: '', [LoginFormKeys.Password]: ''}
     )
     return (
         <section id="login-page" className="auth">
@@ -14,13 +20,13 @@ export default function Login() {
                 <input
                     type="email"
                     id="email"
-                    name="email"
+                    name={LoginFormKeys.Email}
                     placeholder="Sokka@gmail.com"
                     onChange={onChange}
-                    value={values.email}
+                    value={values[LoginFormKeys.Email]}
                 />
                 <label htmlFor="login-pass">Password:</label>
-                <input type="password" id="login-password" name="password" onChange={onChange} value={values.password}/>
+                <input type="password" id="login-password" name={LoginFormKeys.Password} onChange={onChange} value={values[LoginFormKeys.Password]}/>
                 <input type="submit" className="btn submit" defaultValue="Login" />
                 <p className="field">
                     <span>

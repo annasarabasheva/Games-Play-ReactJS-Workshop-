7f496303478bb477aff8196da7edd84b13b043bf
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-07-28 18:06:26 +0300

    Add HOC with home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,73 +1,42 @@
-export default function Home() {
-    return (
+import { useEffect, useState } from "react";
+import withAuth from "../../HOC/withAuth";
+import * as gameService from '../../services/gameService';
+import LatestGame from "./latest-game/LatestGame";
 
+function Home({
+    _id,
+    accessToken,
+    email,
+}) {
+    const [latestGames, setLatestGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getLatest()
+            .then(result => setLatestGames(result));
+    }, [])
+
+    return (
         <section id="welcome-world">
+
             <div className="welcome-message">
                 <h2>ALL new games are</h2>
                 <h3>Only in GamesPlay</h3>
             </div>
             <img src="./images/four_slider_img01.png" alt="hero" />
+
             <div id="home-page">
                 <h1>Latest Games</h1>
-                {/* Display div: with information about every game (if any) */}
-                <div className="game">
-                <div className="image-wrap">
-                    <img src="./images/CoverFire.png" />
-                </div>
-                <h3>Cover Fire</h3>
-                <div className="rating">
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
-                </div>
-                <div className="data-buttons">
-                    <a href="#" className="btn details-btn">
-                    Details
-                    </a>
-                </div>
-                </div>
-                <div className="game">
-                <div className="image-wrap">
-                    <img src="./images/ZombieLang.png" />
-                </div>
-                <h3>Zombie Lang</h3>
-                <div className="rating">
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
-                </div>
-                <div className="data-buttons">
-                    <a href="#" className="btn details-btn">
-                    Details
-                    </a>
-                </div>
-                </div>
-                <div className="game">
-                <div className="image-wrap">
-                    <img src="./images/MineCraft.png" />
-                </div>
-                <h3>MineCraft</h3>
-                <div className="rating">
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
-                </div>
-                <div className="data-buttons">
-                    <a href="#" className="btn details-btn">
-                    Details
-                    </a>
-                </div>
-                </div>
-                {/* Display paragraph: If there is no games  */}
-                <p className="no-articles">No games yet</p>
+
+                {latestGames.map(game => <LatestGame {...game} />)}
+
+                {!latestGames.length && <p className="no-articles">No games yet</p>}
+
+                <p>{email}</p>
             </div>
         </section>
-
     );
 }
+
+const EnhancedHome = withAuth(Home);
+
+export default EnhancedHome;
